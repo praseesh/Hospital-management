bd702bcba290e41ee0fbfd1df8be6d947d527cff
Praseesh <praseeshprasee@gmail.com>
2024-07-10 16:26:03 +0530

    ALLOWED_HOSTS

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m*t92g4tk#=%5b5@+)i-@abwx0go3j2!tjdu53k-oqj8+b6r$b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
