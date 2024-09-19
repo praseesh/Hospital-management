2251bb2af7f4dd75b032b821ce864975223c0d73
Praseesh <praseeshprasee@gmail.com>
2024-09-19 21:22:54 +0530

    some tests added

--- staff/tests.py
+++ staff/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.hashers import make_password
+from .models import Staff  # or Doctor, depending on the login scenario
 
-# Create your tests here.
+class LoginViewTest(TestCase):
+
+    def test_staff_login_success(self):
+
+        response = self.client.post(self.login_url, {
+            'email': 'prasee@gmail.com',
+            'password': '1234'
+        })
+
+        print('Response status code:', response.status_code)
+        print('Response content:', response.content.decode())  
+        print('Session:', self.client.session.items())  
+
+        self.assertEqual(response.status_code, 302)  
+
+        session = self.client.session
+        self.assertIn('staff_id', session, "Staff ID not found in session")
+        self.assertEqual(session['staff_id'], self.staff.id)
