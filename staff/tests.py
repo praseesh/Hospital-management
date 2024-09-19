9d21cdeba4cc0df6e90e6709280a85c07c42a733
Praseesh <praseeshprasee@gmail.com>
2024-09-19 21:23:54 +0530

    message

--- staff/tests.py
+++ staff/tests.py
@@ -16,7 +16,6 @@ class LoginViewTest(TestCase):
         print('Session:', self.client.session.items())  
 
         self.assertEqual(response.status_code, 302)  
-
         session = self.client.session
         self.assertIn('staff_id', session, "Staff ID not found in session")
         self.assertEqual(session['staff_id'], self.staff.id)
